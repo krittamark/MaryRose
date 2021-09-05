292420b8e66922f9ef0049d0c4993f47a1d18363
krittamark <markkrit41@gmail.com>
2021-09-05 18:03:05 +0700

    Implement UIKits

--- routes/api/sponsors.js
+++ routes/api/sponsors.js
@@ -1,12 +1,11 @@
 module.exports = (req, res) => {
-	
 	if (!req.files || Object.keys(req.files).length === 0) {
 		res.status(400).send({ message: "No files were uploaded." });
 		return;
 	}
 
 	const uploadPath = `${__dirname}/../../uploads/sponsors/${req.files.file.name}`;
-	req.files.file.mv(uploadPath, function (err) {
+	req.files.file.mv(uploadPath, (err) => {
 		if (err) {
 			return res.status(500).send(err);
 		}
