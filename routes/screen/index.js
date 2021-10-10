2c2a2ecb4290ea8c09b9343d506b00be89ca44e7
krittamark <markkrit41@gmail.com>
2021-10-10 13:14:37 +0000

    New

--- routes/screen/index.js
+++ routes/screen/index.js
@@ -1,9 +1,11 @@
 const routes = require('express').Router();
 
 const client = require('./client');
+const clientNew= require('./clientNew');
 const admin = require('./admin');
 
 routes.get('/client', client);
+routes.get('/v2/client', clientNew);
 routes.get('/admin', admin);
 
 module.exports = routes;
